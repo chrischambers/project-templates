3513bfaff075faada83d9ddf13428d599617c8d8
Chris Chambers <magma.chambers@gmail.com>
2026-02-15 12:13:48 +0000

    Better integration for flowstorm

--- resources/chrischambers/guardrails/root/.dir-locals.el
+++ resources/chrischambers/guardrails/root/.dir-locals.el
@@ -1,3 +1,6 @@
 ((clojure-mode . ((cider-preferred-build-tool . clojure-cli)
                   (clojure-guardrails-enabled . t)
-                  (cider-clojure-cli-aliases . ":test/env:dev:guardrails"))))
+                  (cider-clojure-cli-aliases . ":test/env:dev:guardrails:flowstorm")
+                  (cider-jack-in-nrepl-middlewares . ("refactor-nrepl.middleware/wrap-refactor"
+                                                      "cider.nrepl/cider-middleware"
+                                                      "flow-storm.nrepl.middleware/wrap-flow-storm")))))
